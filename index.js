0b5a2dc81af85c963c6fb8f68a72efa63415db32
linlinyang <935704186@qq.com>
2019-04-03 17:17:42 +0800

    plugin build
    插件基本完成

--- index.js
+++ index.js
@@ -19,10 +19,11 @@
     "use strict";
 
     var inBrowser = window !== undefined,
-        supportTouch  = !!document && 'touchend' in document,
+        supportTouch  = !!document && 'ontouchend' in document,
         supportPassive = false,
         captureSupport = false,
-        onceSupport = false;
+        onceSupport = false,
+        toString = Object.prototype.toString;
 
     try{
 
@@ -44,69 +45,139 @@
             }
         })
 
-        window.addEventListener('test',null,option);
+        window.addEventListener('passive-test',null,option);
     }catch(e){}
-    
-    function forceMerge(to,from){
-        var keys = from.keys(),
-            len = keys.length;
 
+    function free(el){
+        if(el._cacheEvent === undefined){
+            el._cacheEvent = [];
+        }
+        var caches = el._cacheEvent,
+            len = caches.length;
         while(len--){
-            var tempKey = keys[len];
-            if(to[tempKey] === undefined){
-                to[tempKey] = from[tempKey];
-            }else{
-                to[tempKey] = to[tempKey] && from[tempKey];
-            }
+            var tempEvent = caches[len];
+            el.removeEventListener(tempEvent.type,tempEvent.handler,tempEvent.options);
         }
 
-        return to;
+        delete el._cacheEvent;
     }
 
     if(!Element.prototype.bindEvent){
-        var oListeners = [];
         Element.prototype.bindEvent = function(type,handler,options){
-            
+            if(this._cacheEvent === undefined){
+                this._cacheEvent = [];
+            }
+            var caches = this._cacheEvent;
+            this.addEventListener(type,handler,options);
+            caches.push({
+                type: type,
+                handler: handler,
+                options: options
+            });
         };
     }
+    
+    var oldX = 0,
+        oldY = 0,
+        startTime = 0,
+        oldEl = null;
+    function createTouchStart(modifiers){
+        return function(e){
+            startTime = new Date();
+            var touch = e.touches && e.touches.length == 1 
+                        ? e.touches[0]
+                        : e;
+            oldX = touch.clientX;
+            oldY = touch.clientY;
+            oldEl = touch.currentTarget;
 
-    var oldX = oldY = startTime = 0;
-    function createTouchStart(modifiers,handlerObj){
-        return function(e){
-            
+            if(modifiers.stop){
+                e.stopPropagation();
+            }
+            if(!modifiers.passive && modifiers.prevent){
+                e.preventDefault();
+            }
+
         };
     }
-    function createTouchMove(modifiers,handlerObj){
-        
-    }
-    function createTouchEnd(modifiers,handlerObj){
+    function createTouchEnd(modifiers,handlerObj,args){
         return function(e){
-            var curEl = e.currentElement,
-                targetEl = e.target || e.srcElement;
-            
-            
+            var now = new Date(),
+                touch = e.changedTouches && e.changedTouches.length == 1
+                        ? e.changedTouches[0]
+                        : e,
+                curX = touch.clientX,
+                curY = touch.clientY,
+                curEl = touch.currentTarget,
+                targetEl = touch.target || touch.srcElement;
+                
+            if(modifiers.stop){
+                e.stopPropagation();
+            }
+            if(!modifiers.passive && modifiers.prevent){
+                e.preventDefault();
+            }
+
+            var disX = Math.abs(curX - oldX),
+                disY = Math.abs(curY - oldY);
+            if(now - startTime < 300){
+                if(disX < 10 && disY < 10){//触发点击事件
+                    if((modifiers.self && oldEl === targetEl && curEl === targetEl) || !modifiers.self){
+                        var exec = typeof handlerObj === 'function'
+                                    ? handlerObj
+                                    : typeof handlerObj.tap === 'function'
+                                        ? handlerObj.tap
+                                        : null;
+                                        
+                        exec && exec.call(targetEl,e,args);
+                        modifiers.once && free(targetEl);
+                    }
+                }
+            }else if(disX > 30 || disY > 30){
+                if(disX > disY){//水平方向
+                    if(curX - oldX > 30){//右滑
+                        console.log('aaa');
+                        typeof handlerObj.tapRight === 'function' && handlerObj.tapRight.call(targetEl,e,args);
+                        modifiers.once && free(targetEl);
+                    }else if(curX - oldX < -30){//左滑
+                        console.log('ss');
+                        typeof handlerObj.tapLeft === 'function' && handlerObj.tapLeft.call(targetEl,e,args);
+                        modifiers.once && free(targetEl);
+                    }
+                }else{//垂直方向
+                    if(curY - oldY > 30){//下滑
+                        typeof handlerObj.tapDown === 'function' && handlerObj.tapDown.call(targetEl,e,args);
+                        modifiers.once && free(targetEl);
+                    }else if(curY - oldY < -30){//上滑
+                        typeof handlerObj.tapUp === 'function' && handlerObj.tapUp.call(targetEl,e,args);
+                        modifiers.once && free(targetEl);
+                    }
+                }
+            }
         };
     }
 
     var vueTap = {
-        bind(el,binding){
-            var handler = binding.value;
-            handler = typeof handler === 'function'
-                        ? handler
-                        : typeof handler.handler === 'function'
-                            ? handler.handler
-                            : null;
-            var modifiers = forceMerge({
-                passive: true,
-                capture: false,
-                once: false,
-                prevent: false,
-                stop: false,
-                self: false
-            },binding.modifiers);
-            el.bindEvent('click',function(){},modifiers);                     
+        bind: function(el,binding){
+            var modifiers = binding.modifiers || Object.create(null),
+                option = supportPassive
+                        ? {
+                            capture: modifiers.capture,
+                            passive: modifiers.passive
+                        }
+                        : modifiers.capture;
+                            
+            free(el);
+            if(supportTouch){
+                el.bindEvent('touchstart',createTouchStart(modifiers),option);
+                el.bindEvent('touchend',createTouchEnd(modifiers,binding.value,binding.arg),option);
+            }else{
+                el.bindEvent('mousedown',createTouchStart(modifiers),option);
+                el.bindEvent('mouseup',createTouchEnd(modifiers,binding.value,binding.arg),option);
+            }
         },
-        unbind(el,binding){
+        unbind: function(el,binding){
+            free(el);
         }
     };
 
@@ -115,13 +186,13 @@
             return ;
         }
         _install.installed = true;
-        vue.directive('tap',vueTap);
+        vue.directive('vueTap',vueTap);
     }
 
     vueTap.version = '1.0.0';
-    vueTap.install = _install;
 
     if(inBrowser && window.Vue){
+        vueTap.install = _install;
         window.Vue.use(vueTap);
     }
 
