f88ef667ff6d1afcb34c3ab0b17fe16d4de4db72
linlinyang <935704186@qq.com>
2019-04-04 16:05:29 +0800

    roll up test

--- build/rollup.dev.config.js
+++ build/rollup.dev.config.js
@@ -1,13 +1,20 @@
 const path = require('path');
+const node = require('rollup-plugin-node-resolve');
+const babel = require('rollup-plugin-babel');
 const resolve = _path => path.resolve(__dirname,'../',_path);
 const {input,output} = require('./config')[0];
 
 //module.export = Object.assign({},input,{output});
-
 export default {
     input: resolve('src/index.js'),
     output: [{
         file: resolve('dist/bundle.js'),
         format: 'umd'
-    }]
+    }],
+    plugins: [
+        node(),
+        babel({
+            exclude: "node_modules/**"
+        })
+    ]
 };
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import {inBrowser,supportTouch} from './utils/dom';
-
-console.log('rollup success');
-console.log(inBrowser);
-console.log(supportTouch);
